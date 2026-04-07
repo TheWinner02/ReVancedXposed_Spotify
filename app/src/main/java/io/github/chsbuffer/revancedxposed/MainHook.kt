4d5fa854c813ea181fd204a474410b8355bb0ae7
TheWinner02 <124472504+TheWinner02@users.noreply.github.com>
2026-04-07 15:03:43 +0200

    Update avatar ID list for Spotify integration

--- app/src/main/java/io/github/chsbuffer/revancedxposed/MainHook.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/MainHook.kt
@@ -52,7 +52,7 @@ class MainHook : IXposedHookLoadPackage, IXposedHookZygoteInit {
                     val decorView = activity.window.decorView as ViewGroup
                     decorView.viewTreeObserver.addOnGlobalLayoutListener {
                         // Proviamo a trovare l'avatar tramite ID comuni
-                        val avatarIds = listOf("profile_button", "entity_media_artwork", "self_view", "avatar", "faceview")
+                        val avatarIds = listOf("profile_button", "profile_image", "avatar", "user_avatar")
                         var found = false
 
                         for (idName in avatarIds) {
